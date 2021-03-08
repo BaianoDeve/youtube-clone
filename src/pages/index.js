57daf39b4413a3f2fbc9b75c4639daf02a301cd6
Renan Alves <coelhoeliteyt@gmail.com>
2021-03-08 09:53:28 -0300

    add material-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       clone do youtube com nextjs + material-ui
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
